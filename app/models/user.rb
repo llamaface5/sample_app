b3fbbf3784ad31ed32621b681d9fd306c73bad89
Jason <jason.r.see@gmail.com>
2011-06-21 21:35:56 -0400

    Added password and encrypted pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 # == Schema Information
-# Schema version: 20110620003919
+# Schema version: 20110622013037
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessor   :password
+	attr_accessible :name, :email, :password, :password_confirmation
 	
 	email_regex = /\A[\w+\-.]+@[a-zA-Z\d\-.]+\.[a-zA-Z]+\z/i
 	
@@ -20,4 +22,8 @@ class User < ActiveRecord::Base
 	validates :email, :presence   => true,
 										:format     => { :with => email_regex },
 										:uniqueness => { :case_sensitive => false }
+	validates :password, :presence => true,
+	                     :confirmation => true,
+	                     :length => { :within => 6..40 }
+
 end
