9c5a3416f80945dffa569d829a7e45f463f49db0
Jason <jason.r.see@gmail.com>
2011-06-24 22:52:07 -0400

    Added a User factory and test for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name                  "Jason See"
+	user. email                "exam@ple.com"
+	user.password              "foobar"
+	user.password_confirmation "foobar"
+end
