1211461445b70e9658680583381d7075ec5ca9ab
Jason <jason.r.see@gmail.com>
2011-06-25 22:29:05 -0400

    added sessions controller and sign in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 SampleApp::Application.routes.draw do
 	
-	resources :users
 
+	resources :users
+	resources :sessions, :only => [:new, :create, :destroy]
+	
 	root :to => "pages#home"
 	
-	match '/about', :to => 'pages#about'
+	match '/about',   :to => 'pages#about'
 	match '/contact', :to => 'pages#contact'
-	match '/help', :to => 'pages#help'
-	match '/signup', :to => 'users#new'
-  
+	match '/help',    :to => 'pages#help'
+	match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
